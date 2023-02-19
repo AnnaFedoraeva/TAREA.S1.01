068fd7bec3a0b6d2e1a1807ce9f0fd1eed2daf6e
AnnaFedoraeva <anafedoraeva@gmail.com>
2023-02-19 12:09:27 +0100

    update

---
+++ Documents/ITACADEMY/SPRINT4/S04T01N02FedoraevaAnna/src/main/java/cat/itacademy/barcelonactiva/fedoraeva/anna/s04/t01/n02/S04T01N02FedoraevaAnna/controller/HelloWorldController.java
@@ -0,0 +1,29 @@
+package cat.itacademy.barcelonactiva.fedoraeva.anna.s04.t01.n02.S04T01N02FedoraevaAnna.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(value = "/HelloWorld", method = RequestMethod.GET)
+	public ResponseEntity<?> saluda(@RequestParam(defaultValue = "UNKNOWN") String nom) {
+		return new ResponseEntity<>("Hola, " + nom + ". Estàs executant un projecte Gradle.", HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = { "/HelloWorld2", "/HelloWorld2/{nom}" })
+	public ResponseEntity<?> saluda2(@PathVariable(required = false) String nom) {
+		if (nom != null) {
+			return new ResponseEntity<>("Hola, " + nom + ". Estàs executant un projecte Gradle.", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("Hola. Estàs executant un projecte Gradle.", HttpStatus.OK);
+		}
+	}
+
+}
---
+++ Documents/ITACADEMY/SPRINT4/S04T01N02FedoraevaAnna/src/test/java/cat/itacademy/barcelonactiva/fedoraeva/anna/s04/t01/n02/S04T01N02FedoraevaAnna/S04T01N02FedoraevaAnnaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.fedoraeva.anna.s04.t01.n02.S04T01N02FedoraevaAnna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N02FedoraevaAnnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
